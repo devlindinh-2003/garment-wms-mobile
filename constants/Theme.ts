1772fe86b1fb08f0746b94e2e972b7e2f73f5c3e
Nguyen Dinh Trung Kien <devlindinh2003@gmail.com>
2024-09-17 14:55:08 +0700

    feat: set up folder structure [KienNDT]

---
+++ constants/Theme.ts
@@ -0,0 +1,37 @@
+const Theme = {
+  primaryLightBackgroundColor: '#0866FF',
+  primaryDarkBackgroundColor: '#070054',
+  secondaryBackgroundColor: '#FFFFFF',
+  primaryTextColor: '#F6F7F8',
+  secondaryTextColor: '#323643',
+  tertiaryTextColor: '#9EA1B1',
+  greyText: '#6b7280',
+  commonBtnLightBackground: '#0866FF',
+  commonBtnDarkBackground: '#070054',
+  blue: {
+    50: '#eff6ff',
+    100: '#dbeafe',
+    200: '#bfdbfe',
+    300: '#93c5fd',
+    400: '#60a5fa',
+    500: '#3b82f6',
+    600: '#2563eb',
+    700: '#1d4ed8',
+    800: '#1e40af',
+    900: '#1e3a8a',
+    950: '#172554',
+  },
+  commonBtnText: '#FFFFFF',
+  btnBackground: '#FFFFFF',
+  btnText: '#0866FF',
+  success: '#15803d',
+  error: '#D11313',
+  information: '#0569FF',
+  pending: '#FE9705',
+  skeleton: {
+    bg: '#adadad',
+    hl: '#cccccc',
+  },
+};
+
+export default Theme;
