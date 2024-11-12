a0bcc575627e6c312557e281710c30eae0de83b3
Nguyen Dinh Trung Kien <devlindinh2003@gmail.com>
2024-11-12 20:31:47 +0700

    chore: create api for inspect request statistic [KienNDT]

--- api/inspectionRequest.ts
+++ api/inspectionRequest.ts
@@ -5,6 +5,8 @@ import {
   FilterOperationType,
 } from '@chax-at/prisma-filter-common';
 import { InspectionRequestListResponse } from '@/types/InspectionRequestResponse';
+import { ApiResponse } from '@/types/ApiResponse';
+import { InspectionRequestType } from '@/enums/inspectionRequestType';
 
 interface GetAllInspectionRequestInput {
   sorting?: { id: string; desc: boolean }[];
@@ -60,5 +62,30 @@ export const getAllnspectionRequest = async ({
   } catch (error) {
     console.error('Error fetching import requests:', error);
     throw new Error('Failed to fetch import requests');
+  }
+};
+
+export const getInspectionStatisticByType = async (
+  type: InspectionRequestType
+): Promise<ApiResponse> => {
+  try {
+    const config = get(`/inspection-request/statistic`, { type });
+    const response = await axios(config);
+    return response.data as ApiResponse;
+  } catch (error: any) {
+    console.error('Failed to fetch inspection statistics:', error);
+
+    if (axios.isAxiosError(error) && error.response) {
+      return {
+        statusCode: error.response.status,
+        data: null,
+        message:
+          error.response.data.message ||
+          'An error occurred while fetching statistics.',
+        errors: error.response.data.errors || null,
+      } as ApiResponse;
+    }
+
+    throw new Error('An unexpected error occurred while fetching statistics.');
   }
 };
