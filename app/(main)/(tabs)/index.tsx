6aa17bbe80899273d38ab7879726a41f47ab9262
Nguyen Dinh Trung Kien <devlindinh2003@gmail.com>
2024-11-23 13:53:53 +0700

    chore: loading spinner dashboard [KienNDT]

--- app/(main)/(tabs)/index.tsx
+++ app/(main)/(tabs)/index.tsx
@@ -1,11 +1,11 @@
-import { View, ScrollView } from 'react-native';
+import { ScrollView, ActivityIndicator } from 'react-native';
 import SafeAreaLayout from '@/components/common/SafeAreaLayout';
-import { Text, Card, TouchableRipple } from 'react-native-paper';
+import { Text, Card } from 'react-native-paper';
 import { FC } from 'react';
-import { router } from 'expo-router';
 import { useGetInspectionStatisticByType } from '@/hooks/useGetImportRequestStatistic';
 import { InspectionRequestType } from '@/enums/inspectionRequestType';
 import MaterialStatistic from '@/components/material/MaterialStatistic';
+import SpinnerLoading from '@/components/common/SpinnerLoading';
 
 type StatusCardProps = {
   count: number;
@@ -20,40 +20,20 @@ const StatusCard: FC<StatusCardProps> = ({ count, status, color }) => (
   </Card>
 );
 
-type SectionHeaderProps = {
-  title: string;
-  total: number;
-  onPress: () => void;
-};
-
-const SectionHeader: FC<SectionHeaderProps> = ({ title, total, onPress }) => (
-  <View className='flex flex-row items-center justify-between my-4'>
-    <View
-      className='flex flex-row items-center space-x-2'
-      style={{ minWidth: 180 }}
-    >
-      <Text variant='titleMedium' className='font-bold'>
-        {title}
-      </Text>
-      <Card className='px-2 py-1 border border-gray-500 rounded-lg'>
-        <Text className='text-gray-700'>
-          Total: <Text className='font-bold'>{total}</Text>
-        </Text>
-      </Card>
-    </View>
-    <TouchableRipple onPress={onPress}>
-      <Text className='text-blue-500 underline'>Details →</Text>
-    </TouchableRipple>
-  </View>
-);
-
 const DashboardPage: FC = () => {
-  const { data: materialData } = useGetInspectionStatisticByType(
-    InspectionRequestType.MATERIAL
-  );
-  const { data: productData } = useGetInspectionStatisticByType(
-    InspectionRequestType.PRODUCT
-  );
+  const { data: materialData, isPending: isPendingMaterial } =
+    useGetInspectionStatisticByType(InspectionRequestType.MATERIAL);
+  const { data: productData, isPending: isPendingProduct } =
+    useGetInspectionStatisticByType(InspectionRequestType.PRODUCT);
+
+  // Show loading spinner when either data is pending
+  if (isPendingMaterial || isPendingProduct) {
+    return (
+      <SafeAreaLayout className='flex-1 justify-center items-center'>
+        <SpinnerLoading />
+      </SafeAreaLayout>
+    );
+  }
 
   return (
     <SafeAreaLayout className='px-4 py-2'>
