570ab5da690795e8be55ebcb0a9cb409a21bb151
Nguyen Dinh Trung Kien <devlindinh2003@gmail.com>
2024-11-24 11:40:27 +0700

    chore: warehouse stsaff auth [KienNDT]

--- api/auth.ts
+++ api/auth.ts
@@ -1,16 +1,12 @@
 import { get, post } from './ApiCaller';
 
 let loginUrl = `/auth/sign-in`;
-// let registerUrl = `/auth/register`;
 let refreshTokenUrl = `/auth/refresh-token`;
-// let GooogleAuth = `/auth/google-oauth`;
 export const authApi = {
   login: (email: string, password: string, role: string) => {
     return post(loginUrl, { email, password, role });
   },
-  // register: (email: string, password: string) => {
-  //     return post(registerUrl, { email, password });
-  // },
+
   refreshToken: (header: any) => {
     return get(refreshTokenUrl, {}, header);
   },
