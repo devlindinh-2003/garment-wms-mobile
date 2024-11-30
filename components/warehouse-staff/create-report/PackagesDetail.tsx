5120f25ecefe1cbb8a36ca18e39a73bbba7047ad
Nguyen Dinh Trung Kien <devlindinh2003@gmail.com>
2024-11-30 21:41:39 +0700

    chore: package detail modal [KienNDT]

--- components/warehouse-staff/create-report/PackagesDetail.tsx
+++ components/warehouse-staff/create-report/PackagesDetail.tsx
@@ -1,8 +1,24 @@
+import AppbarHeader from '@/components/common/AppBarHeader';
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 
-const PackagesDetail = () => {
-  return <View></View>;
+interface PackagesDetailProps {
+  closeModal: () => void;
+  receiptCode: string | null;
+}
+
+const PackagesDetail: React.FC<PackagesDetailProps> = ({
+  closeModal,
+  receiptCode,
+}) => {
+  return (
+    <View>
+      <AppbarHeader title={`${receiptCode}`} onPress={closeModal} />
+      <Text className='text-red-500 font-semibold text-xl'>
+        Packages Details
+      </Text>
+    </View>
+  );
 };
 
 export default PackagesDetail;
--- components/warehouse-staff/create-report/PackagesItem.tsx
+++ components/warehouse-staff/create-report/PackagesItem.tsx
@@ -6,10 +6,11 @@ import {
   CalendarX,
   CalendarPlus,
 } from 'lucide-react-native';
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Modal } from 'react-native';
 import { Card, Divider, Button, TextInput } from 'react-native-paper';
 import { convertDate } from '@/helpers/converDate';
+import PackagesDetail from './PackagesDetail';
 
 interface InventoryReportDetail {
   id: string;
@@ -37,130 +38,162 @@ interface PackagesItemProps {
 }
 
 const PackagesItem: React.FC<PackagesItemProps> = ({ details }) => {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [receiptCode, setReceiptCode] = useState<string | null>(null);
+
+  const openModal = (code: string | null) => {
+    setReceiptCode(code);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+    setReceiptCode(null);
+  };
+
   return (
-    <View className='bg-gray-100'>
-      {details.map((detail, index) => (
-        <Card
-          key={index}
-          className='mb-4 rounded-xl shadow-lg bg-white border border-gray-200'
-        >
-          <Card.Content>
-            {/* Render Receipt Header */}
-            <View className='flex flex-row items-center justify-between mb-4'>
-              <Text className='text-lg font-bold text-blue-700'>
-                {detail.materialReceipt
-                  ? `Material Receipt:`
-                  : `Product Receipt:`}
-              </Text>
-              <StatusBadge>
-                {detail.materialReceipt
-                  ? detail.materialReceipt.code
-                  : detail.productReceipt?.code || 'N/A'}
-              </StatusBadge>
-            </View>
-            <Divider className='mb-4' />
-
-            {/* Render Quantity Details */}
-            <View className='mb-2'>
-              <View className='flex-row items-center justify-between mb-2'>
-                <View className='flex-row items-center gap-2'>
-                  <NotepadText size={20} color={Theme.greyText} />
-                  <Text className='text-sm font-semibold text-gray-700'>
-                    Expected Quantity:
-                  </Text>
-                </View>
-                <Text className='font-bold text-gray-700 text-lg '>
-                  {detail.expectedQuantity}
+    <>
+      <View className='bg-gray-100'>
+        {details.map((detail, index) => (
+          <Card
+            key={index}
+            className='mb-4 rounded-xl shadow-lg bg-white border border-gray-200'
+          >
+            <Card.Content>
+              {/* Render Receipt Header */}
+              <View className='flex flex-row items-center justify-between mb-4'>
+                <Text className='text-lg font-bold text-blue-700'>
+                  {detail.materialReceipt
+                    ? `Material Receipt:`
+                    : `Product Receipt:`}
                 </Text>
+                <StatusBadge>
+                  {detail.materialReceipt
+                    ? detail.materialReceipt.code
+                    : detail.productReceipt?.code || 'N/A'}
+                </StatusBadge>
               </View>
-            </View>
+              <Divider className='mb-4' />
 
-            {/* Render Material Receipt Information */}
-            {detail.materialReceipt && (
-              <View className='mb-4'>
+              {/* Render Quantity Details */}
+              <View className='mb-2'>
                 <View className='flex-row items-center justify-between mb-2'>
                   <View className='flex-row items-center gap-2'>
-                    <CalendarX size={20} color={Theme.greyText} />
-                    <Text className='text-sm font-semibold text-gray-700 '>
-                      Expire Date:
-                    </Text>
-                  </View>
-                  <Text className='text-sm text-gray-700'>
-                    {convertDate(detail.materialReceipt.expireDate)}
-                  </Text>
-                </View>
-                <View className='flex-row items-center justify-between'>
-                  <View className='flex-row items-center gap-2'>
-                    <CalendarPlus size={20} color={Theme.greyText} />
+                    <NotepadText size={20} color={Theme.greyText} />
                     <Text className='text-sm font-semibold text-gray-700'>
-                      Import Date:
+                      Expected Quantity:
                     </Text>
                   </View>
-                  <Text className='text-sm text-gray-700'>
-                    {convertDate(detail.materialReceipt.importDate)}
+                  <Text className='font-bold text-gray-700 text-lg '>
+                    {detail.expectedQuantity}
                   </Text>
                 </View>
-                <Divider className='my-4' />
               </View>
-            )}
 
-            {/* Render Product Receipt Information */}
-            {detail.productReceipt && (
+              {/* Render Material Receipt Information */}
+              {detail.materialReceipt && (
+                <View className='mb-4'>
+                  <View className='flex-row items-center justify-between mb-2'>
+                    <View className='flex-row items-center gap-2'>
+                      <CalendarX size={20} color={Theme.greyText} />
+                      <Text className='text-sm font-semibold text-gray-700 '>
+                        Expire Date:
+                      </Text>
+                    </View>
+                    <Text className='text-sm text-gray-700'>
+                      {convertDate(detail.materialReceipt.expireDate)}
+                    </Text>
+                  </View>
+                  <View className='flex-row items-center justify-between'>
+                    <View className='flex-row items-center gap-2'>
+                      <CalendarPlus size={20} color={Theme.greyText} />
+                      <Text className='text-sm font-semibold text-gray-700'>
+                        Import Date:
+                      </Text>
+                    </View>
+                    <Text className='text-sm text-gray-700'>
+                      {convertDate(detail.materialReceipt.importDate)}
+                    </Text>
+                  </View>
+                  <Divider className='my-4' />
+                </View>
+              )}
+
+              {/* Render Product Receipt Information */}
+              {detail.productReceipt && (
+                <View className='mb-4'>
+                  <View className='flex-row items-center justify-between mb-2'>
+                    <View className='flex-row items-center gap-2'>
+                      <CalendarX size={20} color={Theme.greyText} />
+                      <Text className='text-sm font-semibold text-gray-700'>
+                        Expire Date:
+                      </Text>
+                    </View>
+                    <Text className='text-sm text-gray-700'>
+                      {convertDate(detail.productReceipt.expireDate)}
+                    </Text>
+                  </View>
+                  <View className='flex-row items-center justify-between'>
+                    <View className='flex-row items-center gap-2'>
+                      <CalendarPlus size={20} color={Theme.greyText} />
+                      <Text className='text-sm font-semibold text-gray-700'>
+                        Import Date:
+                      </Text>
+                    </View>
+                    <Text className='text-sm text-gray-700'>
+                      {convertDate(detail.productReceipt.importDate)}
+                    </Text>
+                  </View>
+                  <Divider className='my-4' />
+                </View>
+              )}
+
+              {/* Render Actual Quantity with Disabled TextInput */}
               <View className='mb-4'>
-                <View className='flex-row items-center justify-between mb-2'>
-                  <View className='flex-row items-center gap-2'>
-                    <CalendarX size={20} color={Theme.greyText} />
-                    <Text className='text-sm font-semibold text-gray-700'>
-                      Expire Date:
-                    </Text>
-                  </View>
-                  <Text className='text-sm text-gray-700'>
-                    {convertDate(detail.productReceipt.expireDate)}
+                <View className='flex-row items-center justify-between gap-3'>
+                  <Text className='text-sm font-semibold text-gray-700 '>
+                    Actual Quantity:
                   </Text>
+                  <TextInput
+                    mode='outlined'
+                    value={detail.actualQuantity?.toString() || 'NOT YET'}
+                    disabled
+                    className='flex-1'
+                  />
                 </View>
-                <View className='flex-row items-center justify-between'>
-                  <View className='flex-row items-center gap-2'>
-                    <CalendarPlus size={20} color={Theme.greyText} />
-                    <Text className='text-sm font-semibold text-gray-700'>
-                      Import Date:
-                    </Text>
-                  </View>
-                  <Text className='text-sm text-gray-700'>
-                    {convertDate(detail.productReceipt.importDate)}
-                  </Text>
-                </View>
-                <Divider className='my-4' />
               </View>
-            )}
 
-            {/* Render Actual Quantity with Disabled TextInput */}
-            <View className='mb-4'>
-              <View className='flex-row items-center justify-between gap-3'>
-                <Text className='text-sm font-semibold text-gray-700 '>
-                  Actual Quantity:
-                </Text>
-                <TextInput
-                  mode='outlined'
-                  value={detail.actualQuantity?.toString() || 'NOT YET'}
-                  disabled
-                  className='flex-1'
-                />
-              </View>
-            </View>
+              {/* Render Open Button */}
+              <Button
+                icon='clipboard-arrow-right'
+                mode='contained'
+                onPress={() =>
+                  openModal(
+                    detail.materialReceipt?.code ||
+                      detail.productReceipt?.code ||
+                      ' '
+                  )
+                }
+                className='bg-blue-600'
+              >
+                Open
+              </Button>
+            </Card.Content>
+          </Card>
+        ))}
+      </View>
 
-            {/* Render Open Button */}
-            <Button
-              icon='clipboard-arrow-right'
-              mode='contained'
-              onPress={() => console.log('Open Button Pressed')}
-              className='bg-blue-600'
-            >
-              Open
-            </Button>
-          </Card.Content>
-        </Card>
-      ))}
-    </View>
+      {/* Modal Implementation */}
+      <Modal
+        presentationStyle='pageSheet'
+        animationType='slide'
+        transparent={false}
+        visible={isModalVisible}
+        onRequestClose={closeModal}
+      >
+        <PackagesDetail closeModal={closeModal} receiptCode={receiptCode} />
+      </Modal>
+    </>
   );
 };
 
