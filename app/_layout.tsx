50da2ad7bfad17d7baa5378177b9ce0700ee4677
Trung Kien Nguyễn Đình <devlindinh2003@gmail.com>
2024-12-15 13:22:39 +0700

    hotfix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,7 +10,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import SnackbarComponent from '@/components/common/SnackBar';
 
-SplashScreen.preventAutoHideAsync();
+// SplashScreen.preventAutoHideAsync();
 const queryClient = new QueryClient();
 
 // Create Snackbar Context
